33adfe2857512722498c451af1eb1da0c59c254a
julio-DevI <julio.a.valenca@gmail.com>
2024-02-06 23:55:40 -0300

    tentativa de achar a maior probabilidade e a menor e substituir

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Analizador analizador = new Analizador();
         //int[] valores = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        //double[] probs = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        //double[] probs = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9};
         int[] valores = {3,1,4,1};
         double[] probs = {0.5, 0.9, 0.2, 0.6};
 
         int[] resultado = analizador.acha_sequencia(probs ,valores);
         for (int i = 0; i < resultado.length; i++) {
-            System.out.println(resultado[i]);
+            System.out.print("["+resultado[i]+"]");
         }
     }
 
--- src/main/analiz/Analizador.java
+++ src/main/analiz/Analizador.java
@@ -4,31 +4,52 @@ package analiz;
  */
 public class Analizador {
     public int[] acha_sequencia(double[] probs, int[] valores){
-        double primeira_parte = 0;
         double maior_prob = probs[0];
         double menor_prob = probs[0];
+
+        double maior_valor = valores[0];
+        double menor_valor = valores[0];
+
+        int indice_maior_prob = 0;
+        int indice_menor_prob = 0;
+
+        int indice_maior_valor = 0;
 
         //Verificando a maior probabilidade do array
         for (int i=1; i < probs.length; i++)
         {
              if (probs[i] > maior_prob) {
                  maior_prob = probs[i];
+                 indice_maior_prob = i;
+             }
+
+             if (probs[i] < menor_prob) {
+                menor_prob = probs[i];
+                indice_menor_prob = i;
              }
         }
-        //Verificando a menor probabilidade do array
-        for (int i=1; i < probs.length; i++)
-        {
-            if (probs[i] < menor_prob) {
-                menor_prob = probs[i];
+
+        for (int i=1; i < valores.length;i++) {
+            if (valores[i] > maior_valor) {
+                maior_valor = valores[i];
+                indice_maior_valor = i;
+            }
+
+            if (valores[i] < menor_valor) {
+                menor_valor = valores[i];
             }
         }
 
-        for (int i=0; i < probs.length; i++)
-        {
-            primeira_parte = probs[0] * ((1 - probs[1]) * (valores[0] + probs[0] * probs[1]) * ((1 - probs[2]) * (valores[0] + valores[1])) + (probs[0] * probs[1] * probs[2]));
-            double segunda_parte = primeira_parte * ((1 - probs[3]) * (valores[0] + valores[1] + valores[2]) + (probs[0] * probs[1] * probs[2] * probs[3]) * (valores[0] + valores[1] + valores[2] + valores[3]));
-            System.out.println(segunda_parte);
-        }
-        return new int[valores.length];
+        int maior_prob_int = (int) maior_prob;
+        int menor_prob_int = (int) menor_prob;
+        int maior_valor_int = (int) maior_valor;
+        int menor_valor_int = (int) menor_valor;
+
+        System.out.println(indice_maior_valor);
+
+        // Criando e retornando um array de inteiros contendo os valores encontrados
+        int[] resultados = {maior_prob_int,maior_valor_int, menor_valor_int,menor_prob_int};
+
+        return resultados;
     }
 }
